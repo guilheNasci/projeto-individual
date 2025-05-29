6533f35b9b8055e805e6528ec036ae8e56508e7a
guilheNasci <guilherme.mnascimento@sptech.school>
2025-05-29 11:47:50 -0300

    Inserção de capa dos álbums funcionando.

--- Projeto/src/routes/albums.js
+++ Projeto/src/routes/albums.js
@@ -1,18 +1,14 @@
 var express = require("express");
 var router = express.Router();
-const upload = require('../config/configUpload'); // ARQUIVO COM A CONFIGURAÇÃO DO UPLOAD
-
-
+const upload = require('../config/configUpload');
 var albumController = require("../controllers/albumController");
 
-router.post("/adicionar", function (req, res){
-+    albumController.adicionar(req, res);
+router.post("/adicionar", upload.single('capa_album'), function (req, res){
+    albumController.adicionar(req, res);
 });
 
-
-router.get("/listarPorUsuario/:idUsuario", upload.single('capa_album'),function (req, res) {
+router.get("/listarPorUsuario/:idUsuario", function (req, res) {
     albumController.listarPorUsuario(req, res);
 });
-
 
 module.exports = router;
